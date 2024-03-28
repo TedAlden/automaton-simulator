1f284b403fe042515ee145aa4fbc24e512c32971
Ted <t.aldentempleman@gmail.com>
2024-03-28 23:45:42 +0000

    Refactor main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+var stateContextMenu = document.getElementById('state-context-menu');
+var transitionContextMenu = document.getElementById('transition-context-menu');
+var bodyContextMenu = document.getElementById('body-context-menu');
+var numStates = 0;
+var model = new NFA();
+
 // https://stackoverflow.com/a/2117523
 function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -48,16 +54,95 @@ function addStateElementToDiagram (instance, state, x, y) {
     numStates++;
 }
 
-var stateContextMenu = document.getElementById('state-context-menu');
-var transitionContextMenu = document.getElementById('transition-context-menu');
-var bodyContextMenu = document.getElementById('body-context-menu');
-var numStates = 0;
+// State context menu handler
+$("#diagram").on("contextmenu", ".control", function (event) {
+    event.preventDefault();
+    window.selectedControl = $(this).attr("id");
+    stateContextMenu.style.left = event.pageX + 'px'
+    stateContextMenu.style.top = event.pageY + 'px'
+    stateContextMenu.style.display = 'block'
+    transitionContextMenu.style.display = "none";
+    bodyContextMenu.style.display = "none";
+});
 
-var model = new NFA();
+// Background context menu handler
+$("#diagram").on("contextmenu", function (event) {
+    if (event.target.id == "diagram") {
+        event.preventDefault();
+        bodyContextMenu.style.left = event.pageX + 'px'
+        bodyContextMenu.style.top = event.pageY + 'px'
+        bodyContextMenu.style.display = 'block'
+        transitionContextMenu.style.display = "none";
+        stateContextMenu.style.display = "none";
+    }
+});
+
+// Hide context menus when anywhere is left clicked
+$(document).bind("click", function (event) {
+    $("div.custom-menu").remove();
+    stateContextMenu.style.display = 'none'
+    transitionContextMenu.style.display = "none";
+    bodyContextMenu.style.display = "none";
+});
+
+// Context menu -> Delete state
+$(".context-menu").on("click", ".delete-state", function (event) {
+    instance.remove(window.selectedControl);
+});
+
+// Context menu -> Rename state
+$(".context-menu").on("click", ".rename-state", function (event) {
+    prompt("Rename state");
+});
+
+// Context menu -> Toggle accepting state
+$(".context-menu").on("click", ".toggle-accepting-state", function (event) {
+    let state = document.getElementById(window.selectedControl);
+    let stateName = state.innerHTML;
+    if (model.acceptStates.includes(stateName)) {
+        state.classList.remove("accepting");
+        model.acceptStates.splice(model.acceptStates.indexOf(stateName));
+    } else {
+        state.classList.add("accepting");
+        model.acceptStates.push(stateName);
+    }
+});
+
+// Context menu -> Make starting state
+$(".context-menu").on("click", ".make-starting-state", function (event) {
+    let oldStart = document.querySelector(`[data-state-name='${model.startState}']`);
+    let state = document.getElementById(window.selectedControl);
+    let stateName = state.innerHTML;
+    if (!state.classList.contains("starting")) {
+        state.classList.add("starting");
+        model.setStartState(stateName);
+        if (oldStart !== null) {
+            oldStart.classList.remove("starting");
+        }
+    }
+});
+
+// Context menu -> Delete transition
+$(".context-menu").on("click", ".delete-transition", function (event) {
+    instance.deleteConnection(window.selectedConnection);
+});
+
+// Context menu -> Edit transition
+$(".context-menu").on("click", ".edit-transition", function (event) {
+    prompt("Edit transitions.");
+});
+
+// Context menu -> Create state
+$(".context-menu").on("click", ".create-control", function (event) {
+    let rect = instance.getContainer().getBoundingClientRect();
+    let x = event.pageX - rect.left;
+    let y = event.pageY - rect.top;
+    const state = createStateElement(uuidv4(), "s"+numStates);
+    addStateElementToDiagram(instance, state, x, y);
+});
 
 instance = jsPlumb.getInstance({});
 instance.setContainer("diagram");
-
 instance.registerConnectionTypes({
     "default-connection": {
         // anchor:[ "Perimeter", { shape:"Circle"} ],
@@ -117,100 +202,22 @@ instance.bind("connection", function (info) {
     }
 });
 
+// Transition context menu handler
+instance.bind("contextmenu", function (component, event) {
+    if (component.hasClass("jtk-connector")) {
+        event.preventDefault();
+        window.selectedConnection = component;
+        transitionContextMenu.style.left = event.pageX + 'px'
+        transitionContextMenu.style.top = event.pageY + 'px'
+        transitionContextMenu.style.display = 'block'
+        stateContextMenu.style.display = "none";
+        bodyContextMenu.style.display = "none";
+    }
+});
+
 instance.bind("ready", function () {
     // Create an initial starting state
     const state1 = createStateElement(uuidv4(), "s"+numStates);
-    addStateElementToDiagram(instance, state1, 100, 100);
-
-    // Transition context menu handler
-    instance.bind("contextmenu", function (component, event) {
-        if (component.hasClass("jtk-connector")) {
-            event.preventDefault();
-            window.selectedConnection = component;
-            transitionContextMenu.style.left = event.pageX + 'px'
-            transitionContextMenu.style.top = event.pageY + 'px'
-            transitionContextMenu.style.display = 'block'
-            stateContextMenu.style.display = "none";
-            bodyContextMenu.style.display = "none";
-        }
-    });
-    
-    // State context menu handler
-    $("body").on("contextmenu", "#diagram .control", function (event) {
-        event.preventDefault();
-        window.selectedControl = $(this).attr("id");
-        stateContextMenu.style.left = event.pageX + 'px'
-        stateContextMenu.style.top = event.pageY + 'px'
-        stateContextMenu.style.display = 'block'
-        transitionContextMenu.style.display = "none";
-        bodyContextMenu.style.display = "none";
-    });
-
-    // Default context menu handler
-    $("body").on("contextmenu", function (event) {
-        if (event.target.id == "diagram") {
-            event.preventDefault();
-            bodyContextMenu.style.left = event.pageX + 'px'
-            bodyContextMenu.style.top = event.pageY + 'px'
-            bodyContextMenu.style.display = 'block'
-            transitionContextMenu.style.display = "none";
-            stateContextMenu.style.display = "none";
-        }
-    });
-
-    $(document).bind("click", function (event) {
-        $("div.custom-menu").remove();
-        stateContextMenu.style.display = 'none'
-        transitionContextMenu.style.display = "none";
-        bodyContextMenu.style.display = "none";
-    });
-
-    $("body").on("click", ".delete-state", function (event) {
-        instance.remove(window.selectedControl);
-    });
-
-    $("body").on("click", ".rename-state", function (event) {
-        prompt("Rename state");
-    });
-
-    $("body").on("click", ".toggle-accepting-state", function (event) {
-        let state = document.getElementById(window.selectedControl);
-        let stateName = state.innerHTML;
-        if (model.acceptStates.includes(stateName)) {
-            state.classList.remove("accepting");
-            model.acceptStates.splice(model.acceptStates.indexOf(stateName));
-        } else {
-            state.classList.add("accepting");
-            model.acceptStates.push(stateName);
-        }
-    });
-
-    $("body").on("click", ".make-starting-state", function (event) {
-        let oldStart = document.querySelector(`[data-state-name='${model.startState}']`);
-        let state = document.getElementById(window.selectedControl);
-        let stateName = state.innerHTML;
-        if (!state.classList.contains("starting")) {
-            state.classList.add("starting");
-            model.setStartState(stateName);
-            if (oldStart !== null) {
-                oldStart.classList.remove("starting");
-            }
-        }
-    });
-
-    $("body").on("click", ".delete-transition", function (event) {
-        instance.deleteConnection(window.selectedConnection);
-    });
-
-    $("body").on("click", ".edit-transition", function (event) {
-        prompt("Edit transitions.");
-    });
-
-    $("body").on("click", ".create-control", function (event) {
-        let rect = instance.getContainer().getBoundingClientRect();
-        let x = event.pageX - rect.left;
-        let y = event.pageY - rect.top;
-        const state = createStateElement(uuidv4(), "s"+numStates);
-        addStateElementToDiagram(instance, state, x, y);
-    });
+    addStateElementToDiagram(instance, state1, 100, 100); 
 });
+
